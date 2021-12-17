a34a8de2d3a05bb74d06f0bb3f16c9fe50f74974
Piotr Ługowski <piolug93@gmail.com>
2021-12-17 11:51:32 +0100

    Cleaned

--- log4j-scan.py
+++ log4j-scan.py
@@ -9,21 +9,23 @@
 # ******************************************************************
 
 import argparse
-import random
 import requests
-import time
 import sys
 from urllib import parse as urlparse
-import base64
-import json
 import random
-from uuid import uuid4
-from base64 import b64encode
-from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.PublicKey import RSA
-from Crypto.Hash import SHA256
-from termcolor import cprint
 
+
+bcolors = {
+    'magenta': '\033[95m',
+    'cyan': '\033[96m',
+    'red': '\033[91m',
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'END': '\033[0m',
+}
+
+def cprint(str, color="red"):
+    print(f'{bcolors[color]}{str}{bcolors["END"]}')
 
 # Disable SSL warnings
 try:
@@ -43,9 +45,9 @@ if len(sys.argv) <= 1:
 
 
 default_headers = {
-    'User-Agent': 'log4j-scan (https://github.com/mazen160/log4j-scan)',
+    # 'User-Agent': 'log4j-scan',
     # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
-    'Accept': '*/*'  # not being tested to allow passing through checks on Accept header in older web-servers
+    # 'Accept': '*/*'  # not being tested to allow passing through checks on Accept header in older web-servers
 }
 post_data_parameters = ["username", "user", "email", "email_address", "password"]
 timeout = 4
@@ -100,14 +102,10 @@ parser.add_argument("--waf-bypass",
                     dest="waf_bypass_payloads",
                     help="Extend scans with WAF bypass payloads.",
                     action='store_true')
-parser.add_argument("--dns-callback-provider",
-                    dest="dns_callback_provider",
-                    help="DNS Callback provider (Options: dnslog.cn, interact.sh) - [Default: interact.sh].",
-                    default="interact.sh",
-                    action='store')
 parser.add_argument("--custom-dns-callback-host",
                     dest="custom_dns_callback_host",
                     help="Custom DNS Callback Host.",
+                    default="localhost",
                     action='store')
 parser.add_argument("--disable-http-redirects",
                     dest="disable_redirects",
@@ -153,87 +151,6 @@ def generate_waf_bypass_payloads(callback_host, random_string):
     return payloads
 
 
-class Dnslog(object):
-    def __init__(self):
-        self.s = requests.session()
-        req = self.s.get("http://www.dnslog.cn/getdomain.php",
-                         proxies=proxies,
-                         timeout=30)
-        self.domain = req.text
-
-    def pull_logs(self):
-        req = self.s.get("http://www.dnslog.cn/getrecords.php",
-                         proxies=proxies,
-                         timeout=30)
-        return req.json()
-
-
-class Interactsh:
-    # Source: https://github.com/knownsec/pocsuite3/blob/master/pocsuite3/modules/interactsh/__init__.py
-    def __init__(self, token="", server=""):
-        rsa = RSA.generate(2048)
-        self.public_key = rsa.publickey().exportKey()
-        self.private_key = rsa.exportKey()
-        self.token = token
-        self.server = server.lstrip('.') or 'interact.sh'
-        self.headers = {
-            "Content-Type": "application/json",
-        }
-        if self.token:
-            self.headers['Authorization'] = self.token
-        self.secret = str(uuid4())
-        self.encoded = b64encode(self.public_key).decode("utf8")
-        guid = uuid4().hex.ljust(33, 'a')
-        guid = ''.join(i if i.isdigit() else chr(ord(i) + random.randint(0, 20)) for i in guid)
-        self.domain = f'{guid}.{self.server}'
-        self.correlation_id = self.domain[:20]
-
-        self.session = requests.session()
-        self.session.headers = self.headers
-        self.session.verify = False
-        self.session.proxies = proxies
-        self.register()
-
-    def register(self):
-        data = {
-            "public-key": self.encoded,
-            "secret-key": self.secret,
-            "correlation-id": self.correlation_id
-        }
-        res = self.session.post(
-            f"https://{self.server}/register", headers=self.headers, json=data, timeout=30)
-        if 'success' not in res.text:
-            raise Exception("Can not initiate interact.sh DNS callback client")
-
-    def pull_logs(self):
-        result = []
-        url = f"https://{self.server}/poll?id={self.correlation_id}&secret={self.secret}"
-        res = self.session.get(url, headers=self.headers, timeout=30).json()
-        aes_key, data_list = res['aes_key'], res['data']
-        for i in data_list:
-            decrypt_data = self.__decrypt_data(aes_key, i)
-            result.append(self.__parse_log(decrypt_data))
-        return result
-
-    def __decrypt_data(self, aes_key, data):
-        private_key = RSA.importKey(self.private_key)
-        cipher = PKCS1_OAEP.new(private_key, hashAlgo=SHA256)
-        aes_plain_key = cipher.decrypt(base64.b64decode(aes_key))
-        decode = base64.b64decode(data)
-        bs = AES.block_size
-        iv = decode[:bs]
-        cryptor = AES.new(key=aes_plain_key, mode=AES.MODE_CFB, IV=iv, segment_size=128)
-        plain_text = cryptor.decrypt(decode)
-        return json.loads(plain_text[16:])
-
-    def __parse_log(self, log_entry):
-        new_log_entry = {"timestamp": log_entry["timestamp"],
-                         "host": f'{log_entry["full-id"]}.{self.domain}',
-                         "remote_address": log_entry["remote-address"]
-                         }
-        return new_log_entry
-
-
 def parse_url(url):
     """
     Parses the URL.
@@ -266,48 +183,50 @@ def scan_url(url, callback_host):
     if args.waf_bypass_payloads:
         payloads.extend(generate_waf_bypass_payloads(f'{parsed_url["host"]}.{callback_host}', random_string))
     for payload in payloads:
-        cprint(f"[•] URL: {url} | PAYLOAD: {payload}", "cyan")
-        if args.request_type.upper() == "GET" or args.run_all_tests:
-            try:
-                requests.request(url=url,
-                                 method="GET",
-                                 params={"v": payload},
-                                 headers=get_fuzzing_headers(payload),
-                                 verify=False,
-                                 timeout=timeout,
-                                 allow_redirects=(not args.disable_redirects),
-                                 proxies=proxies)
-            except Exception as e:
-                cprint(f"EXCEPTION: {e}")
+        for header in get_fuzzing_headers(payload).items():
+            cprint(f"[•] URL: {url} | PAYLOAD: {payload} | Header: {header}", "cyan")
+            header = dict([header])
+            if args.request_type.upper() == "GET" or args.run_all_tests:
+                try:
+                    requests.request(url=url,
+                                     method="GET",
+                                     params={"v": payload},
+                                     headers=header,
+                                     verify=False,
+                                     timeout=timeout,
+                                     allow_redirects=(not args.disable_redirects),
+                                     proxies=proxies)
+                except Exception as e:
+                    cprint(f"EXCEPTION: {e}")
 
-        if args.request_type.upper() == "POST" or args.run_all_tests:
-            try:
-                # Post body
-                requests.request(url=url,
-                                 method="POST",
-                                 params={"v": payload},
-                                 headers=get_fuzzing_headers(payload),
-                                 data=get_fuzzing_post_data(payload),
-                                 verify=False,
-                                 timeout=timeout,
-                                 allow_redirects=(not args.disable_redirects),
-                                 proxies=proxies)
-            except Exception as e:
-                cprint(f"EXCEPTION: {e}")
+            if args.request_type.upper() == "POST" or args.run_all_tests:
+                try:
+                    # Post body
+                    requests.request(url=url,
+                                     method="POST",
+                                     params={"v": payload},
+                                     headers=get_fuzzing_headers(payload),
+                                     data=header,
+                                     verify=False,
+                                     timeout=timeout,
+                                     allow_redirects=(not args.disable_redirects),
+                                     proxies=proxies)
+                except Exception as e:
+                    cprint(f"EXCEPTION: {e}")
 
-            try:
-                # JSON body
-                requests.request(url=url,
-                                 method="POST",
-                                 params={"v": payload},
-                                 headers=get_fuzzing_headers(payload),
-                                 json=get_fuzzing_post_data(payload),
-                                 verify=False,
-                                 timeout=timeout,
-                                 allow_redirects=(not args.disable_redirects),
-                                 proxies=proxies)
-            except Exception as e:
-                cprint(f"EXCEPTION: {e}")
+                try:
+                    # JSON body
+                    requests.request(url=url,
+                                     method="POST",
+                                     params={"v": payload},
+                                     headers=get_fuzzing_headers(payload),
+                                     json=header,
+                                     verify=False,
+                                     timeout=timeout,
+                                     allow_redirects=(not args.disable_redirects),
+                                     proxies=proxies)
+                except Exception as e:
+                    cprint(f"EXCEPTION: {e}")
 
 
 def main():
@@ -325,16 +244,9 @@ def main():
     dns_callback_host = ""
     if args.custom_dns_callback_host:
         cprint(f"[•] Using custom DNS Callback host [{args.custom_dns_callback_host}]. No verification will be done after sending fuzz requests.")
-        dns_callback_host =  args.custom_dns_callback_host
+        dns_callback_host = args.custom_dns_callback_host
     else:
-        cprint(f"[•] Initiating DNS callback server ({args.dns_callback_provider}).")
-        if args.dns_callback_provider == "interact.sh":
-            dns_callback = Interactsh()
-        elif args.dns_callback_provider == "dnslog.cn":
-            dns_callback = Dnslog()
-        else:
-            raise ValueError("Invalid DNS Callback provider")
-        dns_callback_host = dns_callback.domain
+        raise ValueError("Invalid DNS Callback provider")
 
     cprint("[%] Checking for Log4j RCE CVE-2021-44228.", "magenta")
     for url in urls:
@@ -344,18 +256,6 @@ def main():
     if args.custom_dns_callback_host:
         cprint("[•] Payloads sent to all URLs. Custom DNS Callback host is provided, please check your logs to verify the existence of the vulnerability. Exiting.", "cyan")
         return
-
-    cprint("[•] Payloads sent to all URLs. Waiting for DNS OOB callbacks.", "cyan")
-    cprint("[•] Waiting...", "cyan")
-    time.sleep(int(args.wait_time))
-    records = dns_callback.pull_logs()
-    if len(records) == 0:
-        cprint("[•] Targets do not seem to be vulnerable.", "green")
-    else:
-        cprint("[!!!] Target Affected", "yellow")
-        for i in records:
-            cprint(i, "yellow")
-
 
 if __name__ == "__main__":
     try:
